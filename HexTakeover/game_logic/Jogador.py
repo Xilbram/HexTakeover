23447b6aef26a1d366340cd94b95820c7e7e6543
gix <gibram.g.f@gmail.com>
2023-05-24 04:35:07 -0300

    Remoçao metodos nao usados jogador

--- HexTakeover/game_logic/Jogador.py
+++ HexTakeover/game_logic/Jogador.py
@@ -4,7 +4,6 @@ class Jogador:
         self.__cor_selecao: str = cor_selecao
         self.__turno: bool = False
         self.__id: str = id
-        self.__hexagonos = []
         self.__totalHexagonos = 0
         self.__vencedor = False
 
@@ -25,11 +24,6 @@ class Jogador:
         return self.__id
     def getColorSelecao(self):
         return self.__cor_selecao
-    def getHexagonos(self) -> []:
-        return self.__hexagonos
 
-    def setHexagonos(self, newArr):
-        self.__hexagonos = newArr
 
-    def getHexLen(self):
-        self.__hexagonos[0].getSideLenght()
+
